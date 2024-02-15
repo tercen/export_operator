3c8f0f925b6be6ad5322ec35f461da2a8deea188
Thiago Monteiro <tsmonteiro@gmail.com>
2024-02-15 18:54:40 +0000

    init commit

---
+++ exporter.py
@@ -0,0 +1,187 @@
+from pptx import Presentation
+from pptx.dml.color import RGBColor
+from pptx.util import Inches, Pt
+import datetime
+from io import BytesIO
+from hashlib import md5
+from base64 import b64encode
+from polars import DataFrame
+from os.path import basename
+import subprocess
+import random, string
+
+class Exporter:
+    def __init__(self, tmpFolder=None):
+        self.pages = []
+
+        if tmpFolder is None:
+            self.tmpFolder = "/tmp/" + ''.join(random.choice(string.ascii_letters) for _ in range(15))
+        else: 
+            self.tmpFolder = tmpFolder
+
+
+    def add_blank_page(self):
+        return -1
+
+    # Image info is a list [imgPath, imgSize]
+    def add_image(self, imgInfo, page_idx=0):
+        pass
+
+    def add_title(self, title, page_idx=0):
+        pass
+
+    def add_text(self, textFile, page_idx=0, text_size=11):
+        pass
+
+    def get_bytes(self):
+        return None
+    
+    def as_dataframe(self):
+        return None
+
+
+class PPTXExporter(Exporter):
+    def __init__(self, output="pptx", tmpFolder=None):
+        super().__init__(tmpFolder=tmpFolder)
+        self.output = output
+        self.presentation = Presentation()
+
+    def add_blank_page(self, stpName=None):
+        blank_slide_layout = self.presentation.slide_layouts[6]
+        self.pages.append(self.presentation.slides.add_slide(blank_slide_layout))
+
+        return len(self.pages)
+    
+    def add_title(self, title, page_idx=None):
+        if page_idx == None:
+            page_idx = len(self.pages)-1
+
+        left = Inches(0)
+        width = Inches(10)
+        text_size = 16
+        height = Inches(2)
+        top = Inches(0.25)
+        
+        txBox = self.pages[page_idx].shapes.add_textbox(left, top, width, height)
+        txBox.text_frame.text = title
+        txBox.text_frame.paragraphs[0].font.size = Pt(text_size)
+        
+        txBox.text_frame.paragraphs[0].font.bold = True
+
+    
+    def add_footer(self, page_idx=None):
+        if page_idx == None:
+            page_idx = len(self.pages)-1
+
+        now = datetime.datetime.now().strftime("%H:%M:%S --- %d, %B %Y")
+
+        text_size = 10
+        left = Inches(0)
+        width = Inches(10)
+        height = Inches(2)
+        top = Inches(7.1)
+        
+        txBox = self.pages[page_idx].shapes.add_textbox(left, top, width, height)
+        run = txBox.text_frame.paragraphs[0].add_run()
+        run.text =  now
+        run.font.color.rgb = RGBColor(128, 128, 128)
+        txBox.text_frame.paragraphs[0].font.size = Pt(text_size)
+
+
+        top = Inches(7)
+        left = Inches(8.48)
+        height = Inches(0.5)
+
+        self.pages[page_idx].shapes.add_picture("img/tercen.png", left, top, height=height)
+
+    def add_image(self, imgInfo, page_idx=None):
+        if page_idx is None:
+            page_idx = len(self.pages)-1
+        
+        # Slide is 10 x 7.5 in. (w x h)
+        top = Inches(0.75)
+        left = Inches(0.5)
+        height = Inches(5.8)
+        #pic = 
+        self.pages[page_idx].shapes.add_picture(imgInfo[0], left, top, height=height)
+
+ 
+
+    def add_text(self, textFile, page_idx=0, text_size=11):
+
+        with open(textFile, "r") as file:
+            text = file.read()
+
+        left = Inches(0)
+        width = Inches(10)
+        height = Inches(5.8)
+        top = Inches(1.5)
+
+        txBox = self.pages[page_idx].shapes.add_textbox(left, top, width, height)
+
+        txBox.text_frame.text = text
+        txBox.text_frame.paragraphs[0].font.size = Pt(text_size)
+
+
+
+
+    def get_bytes(self):
+        buf = BytesIO()
+        self.presentation.save(buf)
+        return buf
+    
+    def as_dataframe(self, filename):
+        
+        self.presentation.save(self.tmpFolder + "/" + basename(filename) + ".pptx")
+        
+        
+
+        if self.output == "pptx":
+            outname = basename(filename) + ".pptx"
+            mimetype = "application/vnd.ms-powerpoint"
+            with open(self.tmpFolder + "/" + basename(filename) + ".pptx", "rb") as file:
+                fileBytes = file.read()
+        else:
+            outname = basename(filename) + ".pdf"
+            mimetype = "application/pdf"
+            subprocess.call(["libreoffice", "--headless" ,\
+                            "--convert-to", "pdf", \
+                            self.tmpFolder + "/" + basename(filename) + ".pptx", \
+                            "--outdir", self.tmpFolder])
+            with open(self.tmpFolder + "/" + basename(filename) + ".pdf", "rb") as file:
+                fileBytes = file.read()
+        
+        checksum = md5(fileBytes).hexdigest()
+
+        imgDf = DataFrame({\
+            ".ci":[0],\
+            "filename":[outname],\
+            "mimetype":[mimetype],\
+            "checksum":[checksum],\
+            ".content":[str(b64encode(fileBytes).decode("utf-8"))]\
+            })
+        return imgDf
+    
+
+# def image_file_to_df(file_path):
+#     checksum = hashlib.md5(open(file_path,'rb').read()).hexdigest()
+
+#     output_str = []
+
+#     for fpath in file_path:
+#         with open(file_path, mode="rb") as f:
+#             fc = f.read()
+#             output_str.append([base64.b64encode(fc)])
+
+
+#     o = output_str[0][0]
+
+#     outs = o.decode('utf-8')
+#     imgDf = pd.DataFrame({
+#         "filename":[filename],
+#         "mimetype":[mimetype],
+#         "checksum":[checksum],
+#         ".content":[outs]
+#     })
+
+#     return imgDf
--- main.py
+++ main.py
@@ -1,16 +1,145 @@
-from tercen.client import context as ctx
-import numpy as np
+from tercen.client import context as context
+from tercen.model.impl import SimpleRelation, CompositeRelation, RenameRelation
+import tercen.http.HttpClientService as http
 
-tercenCtx = ctx.TercenContext()
+from pptx import Presentation
+from pptx.util import Inches
 
-df = (
-    tercenCtx
-    .select(['.y', '.ci', '.ri'], df_lib="pandas")
-    .groupby(['.ci','.ri'], as_index=False)
-    .mean()
-    .rename(columns={".y":"mean"})
-    .astype({".ci": np.int32, ".ri": np.int32})
-)
+from PIL import Image
 
-df = tercenCtx.add_namespace(df) 
-tercenCtx.save(df)
+import zipfile, io, base64, subprocess,string, random, os, shutil
+import time
+
+from exporter import PPTXExporter
+#http://127.0.0.1:5400/test/w/310ae60ad93ec799406fcc2404141831/ds/785e21c9-acd7-4967-a37a-2dff81ce3cf3
+
+def get_simple_relation_id_list(obj):
+    idList = []
+
+    if isinstance(obj, SimpleRelation):
+        idList.extend([obj.id])
+    elif isinstance(obj, CompositeRelation):
+        idList.extend(get_simple_relation_id_list(obj.mainRelation))
+        idList.extend(get_simple_relation_id_list(obj.joinOperators))
+
+    elif isinstance(obj, RenameRelation):
+        cRel = obj.relation
+        idList.extend(get_simple_relation_id_list(cRel.mainRelation))
+        idList.extend(get_simple_relation_id_list(cRel.joinOperators))
+    elif isinstance(obj, list):
+        # Assumed: List of JoinOperator!
+        for o in obj:
+            idList.extend(get_simple_relation_id_list(o.rightRelation))
+
+    return idList
+
+def get_plot_schemas(steps):
+    schemas = {}
+    for stp in steps:
+        if hasattr(stp, "computedRelation"):
+            relationIds = get_simple_relation_id_list(stp.computedRelation)
+            for i in range(0, len(relationIds)):
+                schema = ctx.context.client.tableSchemaService.get(relationIds[i])
+                if any([c.name == ".content" for c in schema.columns]) and any([c.name == "mimetype" for c in schema.columns]):
+                    schemas[stp.name] = schema
+    
+    return schemas
+def random_string(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
+def table_to_file(ctx, schema, tmpFolder=None):
+    mimetypeTbl = http.decodeTSON(ctx.context.client.tableSchemaService.selectStream(schema.id, ["mimetype"], 0, -1))
+    ctt =ctx.context.client.tableSchemaService.selectStream(schema.id, [".content"], 0, -1)
+
+    
+    bytesTbl = http.decodeTSON(ctt)
+    mimetype = mimetypeTbl["columns"][0]["values"][0]
+
+    if tmpFolder is None:
+        tmpFolder = "/tmp/"  + random_string()
+        os.makedirs(tmpFolder)
+    
+    baseImgPath = tmpFolder + "/" + random_string()
+    
+    outImgPath = baseImgPath + ".png"
+
+    print(mimetype)
+
+    if mimetype == "image/svg+xml":
+        saveImgPath = baseImgPath + ".svg"
+
+        with open(saveImgPath, "wb") as file:
+            file.write( base64.b64decode(bytesTbl["columns"][0]["values"][0])  )
+        
+
+        subprocess.call(["inkscape", "-z" ,saveImgPath, "-e", outImgPath])
+
+        im = Image.open(outImgPath)
+        return [outImgPath, im.size, mimetype]
+    
+    if mimetype == "image/png":
+        saveImgPath = baseImgPath + ".png"
+
+        with open(saveImgPath, "wb") as file:
+            file.write( base64.b64decode(bytesTbl["columns"][0]["values"][0])  )
+
+        im = Image.open(saveImgPath)
+        return [saveImgPath, im.size, mimetype]
+    if mimetype == "text/markdown":
+        saveFilePath = baseImgPath + ".txt"
+        with open(saveFilePath, "wb") as file:
+            file.write(base64.b64decode(bytesTbl["columns"][0]["values"][0]))
+
+        return [saveFilePath, None, mimetype]
+
+    return None
+
+
+
+
+#310ae60ad93ec799406fcc2404141831/ds/785e21c9-acd7-4967-a37a-2dff81ce3cf3
+# ctx = context.TercenContext(workflowId="310ae60ad93ec799406fcc2404141831",\
+#                             stepId="785e21c9-acd7-4967-a37a-2dff81ce3cf3")
+ctx = context.TercenContext()
+outputFormat = ctx.operator_property('OutputFormat', typeFn=string, default="PowerPoint (*.pptx)")
+
+workflow = ctx.context.client.workflowService.get(ctx.context.workflowId)
+
+
+tmpFolder = "/tmp/"  + ctx.context.workflowId
+if os.path.exists(tmpFolder):
+    shutil.rmtree(tmpFolder)
+os.makedirs(tmpFolder )
+
+schemas = get_plot_schemas(workflow.steps)
+
+if outputFormat == "PowerPoint (*.pptx)":
+    expo = PPTXExporter(output="pptx", tmpFolder=tmpFolder)
+elif outputFormat == "PDF - Slides (*.pdf)":
+    expo = PPTXExporter(output="pdf", tmpFolder=tmpFolder)
+else:
+    raise ValueError("unsupported format")
+
+for stpName,schema in schemas.items():
+    fileInfo = table_to_file(ctx, schema,  tmpFolder=tmpFolder)
+    
+    if fileInfo[2].startswith("image"):
+    # aspectRatio = imInfo[1][1]/imInfo[1][0]
+        expo.add_blank_page(stpName=stpName)
+        expo.add_image(fileInfo)
+        expo.add_title(stpName)
+        expo.add_footer()
+
+    if fileInfo[2].startswith("text"):
+        expo.add_blank_page(stpName=stpName)
+        expo.add_text(fileInfo[0])
+        expo.add_title(stpName)
+        expo.add_footer()
+
+
+
+imgDf = expo.as_dataframe( "/tmp/"  + ctx.context.workflowId + "/" + workflow.name + "_Report")
+
+
+imgDf = ctx.add_namespace(imgDf)
+ctx.save(imgDf)
